ed8fd63780dd7c3ad5ddfceebeac9a36de52c223
Oli Nelson <olivernelson@icloud.com>
2019-02-01 09:27:56 -0500

    all working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 let dogImageContainer
 let magic
+let allBreeds = []
+let dogBreedResponse
+let searchQuery = ""
 
 
 
@@ -18,11 +21,14 @@ dogBreedContainer = document.querySelector('#dog-breeds')
   .then(response => dogImageDisplay(response))
 
 // ################################
+  function breedApiCall(searchQuery) {
+    fetch( breedUrl )
+    .then(response => response.json())
+    // .then(response => console.log(response.message))
+    .then(response => filterDogBreedDisplay(response, searchQuery))
 
-  fetch( breedUrl )
-  .then(response => response.json())
-  // .then(response => console.log(response.message))
-  .then(response => dogBreedDisplay(response))
+  }
+  breedApiCall(searchQuery)
 
 // #####################################
 
@@ -30,8 +36,13 @@ dogBreedContainer = document.querySelector('#dog-breeds')
     event.target.style.color = "red"
   })
 
+  document.querySelector('#breed-dropdown').addEventListener('change', function(event){
+    // console.log(event.target.value)
+    while (dogBreedContainer.firstChild) dogBreedContainer.removeChild(dogBreedContainer.firstChild);
+    breedApiCall(event.target.value)
+  })
+}) //end of DOMContentLoaded
 
-})
 
 
 function dogImageDisplay(images) {
@@ -43,14 +54,23 @@ function dogImageDisplay(images) {
 }
 
 function dogBreedDisplay(breeds) {
-  magic = breeds
   for (let breed in breeds.message) {
-    // console.log(breed)
+
     dogBreedContainer.innerHTML +=
     `<li>${breed}</li>`
 }
 
+}
 
 
-
+function filterDogBreedDisplay(breeds,searchQueryString) {
+  magic = breeds
+  for (let breed in breeds.message) {
+    // console.log(breed)
+    if (breed.startsWith(searchQueryString)){
+    dogBreedContainer.innerHTML +=
+    `<li>${breed}</li>`
+    allBreeds.push(breed)
+  }
+}
 }
